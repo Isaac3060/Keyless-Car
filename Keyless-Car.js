3f18d574a5ef15477bdd0b34fb5a0b72911cdac8
Isaac3060 <50213336+Isaac3060@users.noreply.github.com>
2020-05-29 23:52:32 -0400

    Create Keyless-Car.js

---
+++ Keyless-Car.js
@@ -0,0 +1,14 @@
+// Ask the user for the age. 
+var age = prompt("What is your age")
+
+if (age < "18"){
+alert("Sorry, you are too young to drive this car. Powering off")
+}
+
+else if ( age==="18"){
+    alert("Congratulations on your first year of driving. Enjoy the ride!")
+}
+
+else{
+alert("Powering on .Enjoy the ride!")
+}
